3f3b412e702caf775c970aacf128270188fb82e4
yigityesilada <yy.yesilada@gmail.com>
2021-03-09 23:22:14 +0300

    ClassMethodDemo

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+	class CustomerManager
+	{
+		public void addCustomer(Customer customer)
+		{
+			Console.WriteLine("New Customer Added: " + customer.Name);
+		}
+
+		public void deleteCustomer(Customer customer)
+		{
+			Console.WriteLine(customer.Id + ". Customer Deleted...");
+
+		}
+		
+		public void getCustomers(Customer[] customers)
+		{
+			foreach (Customer customer in customers)
+			{
+				Console.WriteLine("ID: " + customer.Id);
+				Console.WriteLine("Name: " + customer.Name);
+				Console.WriteLine("Description: " + customer.Surname);
+				Console.WriteLine("UnitPrice: " + customer.Mail);
+				Console.WriteLine("\n");
+			}
+		}
+	}
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Customer customer1 = new Customer();
+			customer1.Id = 1;
+			customer1.Name = "Yiğit";
+			customer1.Surname = "Yeşilada";
+			customer1.Mail = "yy.yesilada@gmail.com";
+
+			Customer customer2 = new Customer();
+			customer2.Id = 2;
+			customer2.Name = "Sema";
+			customer2.Surname = "Cevher";
+			customer2.Mail = "semacevher20@gmail.com";
+
+			Customer[] customers = new Customer[] { customer1,customer2 };
+
+			CustomerManager customerManager = new CustomerManager();
+
+			customerManager.addCustomer(customer1);
+			customerManager.addCustomer(customer2);
+
+			customerManager.deleteCustomer(customer1);
+			customerManager.deleteCustomer(customer2);
+			Console.WriteLine("\n");
+			customerManager.getCustomers(customers);
+		}
+	}
+}
