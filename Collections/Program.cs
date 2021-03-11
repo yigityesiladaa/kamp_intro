2d144edc21eb6d68961c7712ea6df41a0378f9e1
yigityesilada <yy.yesilada@gmail.com>
2021-03-11 23:45:54 +0300

    4. Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> names = new List<string>();
+			names.Add("Yiğit");
+			names.Add("Sema");
+
+
+			foreach (var name in names)
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Product product1 = new Product();
+			product1.Id = 1;
+			product1.CategoryId = 6;
+			product1.ProductName = "Ütü";
+			product1.UnitPrice = 1275;
+			product1.UnitsInStock = 9;
+
+			Product product2 = new Product { Id = 2, CategoryId = 2 ,ProductName = "Masa", UnitPrice = 785,UnitsInStock = 55 };
+
+			ProductManager productManager = new ProductManager();
+		}
+	}
+}
